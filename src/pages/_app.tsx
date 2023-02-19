7a5a747565164c359799a0c13772b3c9ec6ef862
Odukwe Precious <odukwechiemeka@gmail.com>
2023-02-19 08:23:41 +0100

    First commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Montserrat } from '@next/font/google'
+
+const montserrat = Montserrat(
+  { 
+    subsets: ['latin'],
+    weight: ['400'] 
+  }
+)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={montserrat.className}>
+      <Component {...pageProps} />
+    </main>
+  ) 
 }
